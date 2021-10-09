a66311d2ac6583fe614255920105bffdac306b70
b5710546232 <safe.suk@gmail.com>
2021-10-09 16:42:42 +0700

    styling

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 import "./style.css";
+import QRCode from "qrcode";
+import DEFAULT_IMG from "./assets/img/icon.png";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const generateQR = async (text) => {
+  try {
+    const imageData = await QRCode.toDataURL(text, {
+      color: {
+        dark: "#34d399", // Blue dots
+        light: "#0000", // Transparent background
+      },
+    });
+    return imageData;
+  } catch (err) {
+    return null;
+  }
+};
+
+const processQRImage = async (text = "safesuk.dev") => {
+  const imageQr = (await generateQR(text)) || DEFAULT_IMG;
+  const img = document.getElementById("qrcode");
+  img.setAttribute("src", imageQr);
+};
+
+const setText = (text) => {
+  document.getElementById("text").innerText = text;
+};
+
+const handleInputOnchange = (event) => {
+  const value = event.target.value;
+  processQRImage(value);
+  const text = value || "";
+  setText(text);
+};
+
+const init = async () => {
+  processQRImage();
+  const input = document.getElementById("inpuText");
+  input.addEventListener("input", handleInputOnchange);
+};
+
+init();
